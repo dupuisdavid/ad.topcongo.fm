3ee475aa937b496583b6dd796cece7f1d25cefaa
dupuisdavid <ddupuis.pro@gmail.com>
2017-03-19 18:26:09 +0100

    Add test log info

--- ad.topcongo/src/routes.php
+++ ad.topcongo/src/routes.php
@@ -4,6 +4,8 @@
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
+    $this->logger->addInfo("Something interesting happened");
+
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
